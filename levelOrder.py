a50fdb5e8f4da98a2cc990d14e67e109f009ab75
midhun7416 <midhun.nitw@gmail.com>
2014-10-23 12:14:38 -0500

    hi

--- levelOrder.py
+++ levelOrder.py
@@ -17,7 +17,6 @@ class Tree:
     def insert(self, v):
         if self.root is None:
             self.root = Tnode(v)
-            print self.root.val
         else:
             temp = self.root
             while temp is not None:
@@ -34,6 +33,11 @@ class Tree:
             else:
                 par.right = Tnode(v)
 
+    def lca(self, n1, n2):
+        a = self.rec_lca(self.root, n1, n2)
+        if a is not None:
+            print(a.val)
+
     def lorder(self):
         q = Queue.Queue()
         q.put(self.root)
@@ -42,12 +46,12 @@ class Tree:
             curr = q.get()
             if curr.val == -99 and not q.empty():
                 q.put(curr)
-                print "\n",
+                print("\n"),
             elif curr.val == -99 and q.empty():
-                print "\n"
+                print("\n")
                 return
             else:
-                print curr.val,
+                print(curr.val),
                 if curr.left is not None:
                     q.put(curr.left)
                 if curr.right is not None:
@@ -56,9 +60,9 @@ class Tree:
     def vorder(self):
         d = dict()
         self.rec_v_order(self.root, 0, d)
-        print d
+        print(d)
         for l in sorted(d.keys()):
-            print l, ' ', d[l]
+            print (l, ' ', d[l])
 
     def rec_v_order(self, node, level, d):
         if node is None:
@@ -73,10 +77,28 @@ class Tree:
         if node.right is not None:
             self.rec_v_order(node.right, level+1, d)
 
+    def rec_lca(self, curr, n1, n2):
+        if curr is None:
+            return None
+        if (n1 < curr.val and curr.val < n2) or \
+                (n2 < curr.val and curr.val < n1 )\
+                or curr.val == n1 or curr.val == n2:
+            return curr
+        elif n1 < curr.val and n2 < curr.val:
+            return self.rec_lca(curr.left,n1,n2)
+        elif n1 > curr.val and n2 > curr.val:
+            return self.rec_lca(curr.right,n1,n2)
+
+
 if __name__ == '__main__':
     T = Tree()
+    T.insert(5)
     T.insert(3)
     T.insert(1)
-    T.insert(5)
-    T.lorder()
-    T.vorder()
+    T.insert(4)
+    T.insert(7)
+    T.insert(6)
+    T.insert(8)
+    #T.lorder()
+    #T.vorder()
+    T.lca(6, 8)
