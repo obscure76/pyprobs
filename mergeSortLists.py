6e18e9f1b0241441e6ae5fde3698e71d5922c942
midhun7416 <midhun.nitw@gmail.com>
2014-10-23 22:00:35 -0500

    hi

---
+++ mergeSortLists.py
@@ -0,0 +1,13 @@
+__author__ = 'obscure'
+
+class Solution:
+
+    def __init__(self):
+        pass
+
+    def mergeLists(self, l):
+        print(l[0][0])
+
+if __name__ == '__main__':
+    s = Solution()
+    s.mergeLists([[1,3],[2,4]])
