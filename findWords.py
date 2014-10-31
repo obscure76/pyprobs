1872376aeed6cd842545f681896a73d10b5e2dcd
midhun7416 <midhun.nitw@gmail.com>
2014-10-31 12:03:49 -0500

    hi

--- findWords.py
+++ findWords.py
@@ -1,1 +1,27 @@
 __author__ = 'obscure'
+
+class Solution:
+
+    def __init__(self):
+        self.d = {'ab': 1, 'abc':1, 'aes':1, 'owls':1, 'owl':1}
+        self.mat = [['a', 'b', 'c', 'd'],['d', 'e', 'f', 'g'], ['o', 'w', 'l', 's']]
+
+    def getMoves(self, l, r):
+        pass
+
+    def checkword(self, ls, rs, le, re):
+        pass
+    
+    def findWords(self):
+        print(len(self.d))
+        print(len(self.mat))
+        for i in range(0, len(self.mat)):
+            for j in range(0, len(self.mat[i])):
+                moves = self.getMoves(i, j)
+                for move in moves:
+                    pass
+
+
+if __name__ == '__main__':
+    s = Solution()
+    s.findWords()
